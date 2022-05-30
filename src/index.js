6f42216d137d84da6e95a36295b609bfc52a3799
alexmacbook <alex_yasinsky@mail.ru>
2022-05-30 20:59:17 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
-import {diffDates, diffToHtml, handleCalcDates} from "./js/datecalc.js";
+import {handleCalcDates} from "./js/datecalc.js";
 import {routerButtonHandler} from "./js/router.js";
-
-const routerButtons = document.querySelectorAll('.routerButton');
+import handleTimer from './js/timer.js';
 
 const dateCalcForm = document.getElementById("datecalc");
 const dateCalcResult = document.getElementById("datecalc__result");
 
-
-
-const timerForm = document.getElementById('timer');
-const timerResult = document.getElementById('timer__result');
-
 dateCalcForm.addEventListener("submit", (e)=> {
   handleCalcDates(e, dateCalcResult);
 });
+
+const routerButtons = document.querySelectorAll('.routerButton');
 
 routerButtons.forEach( button => {
   button.addEventListener('click', (e)=> {
@@ -22,36 +18,13 @@ routerButtons.forEach( button => {
   })
 });
 
-let timerId;
+const timerForm = document.getElementById('timer');
+const timerResult = document.getElementById('timer__result');
 
 timerForm.addEventListener('submit', (e) => {
   e.preventDefault();
   let action = document.activeElement.value;
-  switch (action) {
-    case 'start':
-      timerResult.innerHTML = '';
-      let formData = new FormData(timerForm);
-      let time = +formData.get('time');
-      timerId = setInterval(() => {
-        timerResult.innerHTML = `${time}`;
-        time -= 1;
-      }, 1000);
-      setTimeout(()=> {
-        const sound = new Howl({
-          src: ['bell-sound.mp3']
-        });
-        sound.play();
-        clearInterval(timerId);
-      }, time*1000+1000);
-      break;
-    case 'stop':
-      clearInterval(timerId);
-      timerResult.innerHTML = '';
-
-      break;
-    default:
-      break;
-  }
+  handleTimer(timerForm, action, timerResult);
 });
 
 
