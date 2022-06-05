de26ec2a852932757f87a8142f9959234141edca
alexmacbook <alex_yasinsky@mail.ru>
2022-06-05 13:26:24 +0300

    task_3 done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,10 @@
-import {handleCalcDates} from "./js/datecalc.js";
-import {routerButtonHandler} from "./js/router.js";
-import handleTimer from './js/timer.js';
 import './styles/style.scss';
 
-const dateCalcForm = document.getElementById("datecalc");
-const dateCalcResult = document.getElementById("datecalc__result");
+import List from "./js/list";
+import {items} from "./js/assets";
 
-dateCalcForm.addEventListener("submit", (e)=> {
-  handleCalcDates(e, dateCalcResult);
-});
+const wrapper = document.querySelector('.gallery__box');
 
-const routerButtons = document.querySelectorAll('.routerButton');
+const gallery = new List(items, wrapper);
 
-routerButtons.forEach( button => {
-  button.addEventListener('click', (e)=> {
-    const pages = {dateCalcForm, timerForm};
-    routerButtonHandler(e.target.value, pages);
-  })
-});
-
-const timerForm = document.getElementById('timer');
-const timerResult = document.getElementById('timer__result');
-
-timerForm.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let action = document.activeElement.value;
-  handleTimer(timerForm, action, timerResult);
-});
-
-
+gallery.render();
